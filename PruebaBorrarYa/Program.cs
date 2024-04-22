45b52d0a9c7c2796f84c725c46afb2c4e8ca4f15
Marcos Chacin <chacinm3@gmail.com>
2024-04-22 12:43:19 +0100

    Agregar archivos de proyecto.

---
+++ PruebaBorrarYa/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PruebaBorrarYa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
